f9d1439db6e401b5da25e3f34fc7d8a56328edb2
Jaegar <118213337+jaegarsaun@users.noreply.github.com>
2023-01-15 21:53:57 -0330

    New features
    Added a Start, Play and Stop button.
    Added more words

--- app.js
+++ app.js
@@ -2,84 +2,84 @@
 
 const fruits = [
   "apple",
-  //   "banana",
-  //   "orange",
-  //   "grape",
-  //   "strawberry",
-  //   "pineapple",
-  //   "watermelon",
-  //   "blueberry",
-  //   "raspberry",
-  //   "kiwi",
-  //   "mango",
-  //   "lemon",
-  //   "lime",
-  //   "coconut",
-  //   "cherry",
-  //   "peach",
-  //   "pear",
-  //   "plum",
-  //   "apricot",
-  //   "avocado",
-  //   "blackberry",
-  //   "cantaloupe",
-  //   "fig",
-  //   "grapefruit",
-  //   "guava",
-  //   "jackfruit",
-  //   "lychee",
-  //   "mandarin",
-  //   "melon",
-  //   "nectarine",
-  //   "papaya",
-  //   "persimmon",
-  //   "pineapple",
-  //   "pomegranate",
-  //   "quince",
-  //   "tangerine",
-  //   "tomato",
-  //   "watermelon",
+  "banana",
+  "orange",
+  "grape",
+  "strawberry",
+  "pineapple",
+  "watermelon",
+  "blueberry",
+  "raspberry",
+  "kiwi",
+  "mango",
+  "lemon",
+  "lime",
+  "coconut",
+  "cherry",
+  "peach",
+  "pear",
+  "plum",
+  "apricot",
+  "avocado",
+  "blackberry",
+  "cantaloupe",
+  "fig",
+  "grapefruit",
+  "guava",
+  "jackfruit",
+  "lychee",
+  "mandarin",
+  "melon",
+  "nectarine",
+  "papaya",
+  "persimmon",
+  "pineapple",
+  "pomegranate",
+  "quince",
+  "tangerine",
+  "tomato",
+  "watermelon",
 ];
-// const action = [
-//   "Standing",
-//   "Reading",
-//   "Singing",
-//   "Flying",
-//   "Watching",
-//   "Sleeping",
-//   "Writing",
-//   "Coloring",
-//   "Drinking",
-//   "Cooking",
-//   "Washing",
-//   "Waiting",
-//   "Eating",
-//   "Talking",
-//   "Laughing",
-//   "Crying",
-//   "Dancing",
-//   "Running",
-//   "Swimming",
-//   "Jumping",
-//   "Walking",
-//   "Playing",
-//   "Smiling",
-//   "Climbing",
-//   "Falling",
-//   "Hiding",
-//   "Hugging",
-//   "Kissing",
-//   "Lying",
-//   "Praying",
-//   "Riding",
-//   "Sitting",
-//   "Sneezing",
-//   "Talking",
-//   "Thinking",
-//   "Waving",
-//   "Whistling",
-//   "Working",
-// ];
+const action = [
+  "Standing",
+  "Reading",
+  "Singing",
+  "Flying",
+  "Watching",
+  "Sleeping",
+  "Writing",
+  "Coloring",
+  "Drinking",
+  "Cooking",
+  "Washing",
+  "Waiting",
+  "Eating",
+  "Talking",
+  "Laughing",
+  "Crying",
+  "Dancing",
+  "Running",
+  "Swimming",
+  "Jumping",
+  "Walking",
+  "Playing",
+  "Smiling",
+  "Climbing",
+  "Falling",
+  "Hiding",
+  "Hugging",
+  "Kissing",
+  "Lying",
+  "Praying",
+  "Riding",
+  "Sitting",
+  "Sneezing",
+  "talking",
+  "thinking",
+  "waving",
+  "whistling",
+  "working",
+];
 
 //Get the HTML elements
 const word = document.getElementById("word");
@@ -93,7 +93,7 @@ let hint;
 
 function displayWord() {
   // Pick a random word
-  let decideWords = Math.floor(Math.random() * 2);
+  let decideWords = Math.floor(Math.random() * 3);
 
   switch (decideWords) {
     case 1: // Fruit Case
@@ -112,34 +112,54 @@ function displayWord() {
       scrambledWord = strarray.join("");
       //Display the word
       word.innerHTML = scrambledWord;
-      
+
       break;
-    // case 2: // Action Case
-    //     let actionWord = action[Math.floor(Math.random() * action.length)];
-    //     break;
+    case 2: // Action Case
+      hint = "An action word!";
+
+      //Pick the word
+      unScrambledWord = action[Math.floor(Math.random() * fruits.length)];
+      unScrambledWord = unScrambledWord.toLowerCase();
+      //Scramble the word
+      strarray = unScrambledWord.split("");
+      var i, j, k;
+      for (i = 0; i < strarray.length; i++) {
+        j = Math.floor(Math.random() * i);
+        k = strarray[i];
+        strarray[i] = strarray[j];
+        strarray[j] = k;
+      }
+      scrambledWord = strarray.join("");
+      //Display the word
+      word.innerHTML = scrambledWord;
+      break;
   }
 }
 //Handle form submit
 function formSubmit() {
-    var userInput = document.getElementById("input").value;
-    userInput.toLowerCase();
-    console.log(userInput);
-    if (userInput === unScrambledWord) {
-      word.innerHTML = 'CORRECT!';
-      document.getElementById("input").value = "";
+  var userInput = document.getElementById("input").value;
+  userInput.toLowerCase();
+  console.log(userInput);
+  if (userInput === unScrambledWord) {
+    word.innerHTML = "CORRECT!";
+    document.getElementById("input").value = "";
+  } else {
+    word.innerHTML = "INCORRECT!";
+    document.getElementById("input").value = "";
+  }
+}
+
+function stopGame() {
+  location.reload();
+}
+function showHint() {
+  if (hintCont.innerHTML === hint) {
+    hintCont.innerHTML = "";
+  } else {
+    if (hint == undefined) {
+      hintCont.innerHTML = "No hint available";
     } else {
-      word.innerHTML = 'INCORRECT!';
-      document.getElementById("input").value = "";
+      hintCont.innerHTML = hint;
     }
   }
-function showHint() {
-    if(hintCont.innerHTML === hint){
-        hintCont.innerHTML = "";
-    }else{
-        if(hint == undefined){
-            hintCont.innerHTML = "No hint available";
-        }else{
-            hintCont.innerHTML = hint;
-        }
-    }
 }
